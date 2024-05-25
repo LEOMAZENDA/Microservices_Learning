7fb4402490c34783b1ef883da26d8a3580956381
LEOMAZENDA <eng.leomazenda@gmail.com>
2024-05-25 15:01:32 -0700

    sessão 7 feita

---
+++ CreekShoping/CreekShoping/CreekShoping.ProductAPI/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using CreekShoping.ProductAPI.Models.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CreekShoping.ProductAPI.Models
+{
+    [Table("TbProduct")]
+    public class Product : BaseEntity
+    {
+        [Required]
+        [Column("name")]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Required]
+        [Column("price")]
+        [Range(1,10000)]
+        public decimal Price { get; set; }
+
+        [Column("description")]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        [Column("category_name")]
+        [StringLength(50)]
+        public string CategoryName { get; set; }
+
+        [Column("image_url")]
+        [StringLength(300)]
+        public string ImageUrl { get; set; }
+    }
+}

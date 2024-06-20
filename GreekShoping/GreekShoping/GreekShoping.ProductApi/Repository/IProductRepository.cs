358ae963cfe4d985bf8039340992fc61c7fdd400
LEOMAZENDA <eng.leomazenda@gmail.com>
2024-06-20 03:35:31 +0100

    Proctu VO e Interface Created

---
+++ GreekShoping/GreekShoping/GreekShoping.ProductApi/Repository/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using GreekShoping.ProductApi.Data.ValueObjects;
+
+namespace GreekShoping.ProductApi.Repository;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<ProductVO>> FindAll();
+    Task<ProductVO> FindById(long id);
+    Task<ProductVO> Create(ProductVO vO);
+    Task<ProductVO> Update(ProductVO vO);
+    Task<bool> Delete(long id);
+}

38b970d46148d49a48f1064df0c664e24f57825b
LEOMAZENDA <eng.leomazenda@gmail.com>
2024-06-20 05:24:43 +0100

    Speeping Products on DataTable

--- GreekShoping/GreekShoping/GreekShoping.ProductApi/Migrations/20240620042257_SpeedProductsDataTable.Designer.cs
+++ GreekShoping/GreekShoping/GreekShoping.ProductApi/Migrations/20240620042257_SpeedProductsDataTable.Designer.cs
@@ -2,6 +2,7 @@
 using GreekShoping.ProductApi.Models.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GreekShoping.ProductApi.Migrations
 {
     [DbContext(typeof(MySqlContext))]
-    partial class MySqlContextModelSnapshot : ModelSnapshot
+    [Migration("20240620042257_SpeedProductsDataTable")]
+    partial class SpeedProductsDataTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- GreekShoping/GreekShoping/GreekShoping.ProductApi/Models/Context/MySqlContext.cs
+++ GreekShoping/GreekShoping/GreekShoping.ProductApi/Models/Context/MySqlContext.cs
@@ -9,4 +9,64 @@ public class MySqlContext : DbContext
     public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) { }
 
     public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().HasData(new Product
+        {
+            Id = 4,
+            Name = "Mascara Star Wars",
+            Description = "Mascara para festas",
+            Price = new decimal(25000.00),
+            CategoryName = "Brinquedos",
+            ImageUrl = "https://github.com/LEOMAZENDA/Microservices_Learning/blob/master/GreekShoping/GreekShoping/ShoppingImages/3_vader.jpg?raw=true",
+        });
+        modelBuilder.Entity<Product>().HasData(new Product
+        {
+            Id = 5,
+            Name = "Camisola Preta Gammer",
+            Description = "Camisola de homem para Jovens e Adultos ",
+            Price = new decimal(499.99),
+            CategoryName = "Roupa",
+            ImageUrl = "https://github.com/LEOMAZENDA/Microservices_Learning/blob/master/GreekShoping/GreekShoping/ShoppingImages/5_100_gamer.jpg?raw=true",
+        });
+        modelBuilder.Entity<Product>().HasData(new Product
+        {
+            Id = 6,
+            Name = "Camisola Branca e Preta SpaceX",
+            Description = "Camisola de homem para Jovens e Adultos ",
+            Price = new decimal(7500.90),
+            CategoryName = "Roupa",
+            ImageUrl = "https://github.com/LEOMAZENDA/Microservices_Learning/blob/master/GreekShoping/GreekShoping/ShoppingImages/6_spacex.jpg?raw=true",
+        });
+        modelBuilder.Entity<Product>().HasData(new Product
+        {
+            Id = 7,
+            Name = "Camisola Preta - Occupy Mar",
+            Description = "Camisola de homem para Jovens e Adultos ",
+            Price = new decimal(7500.90),
+            CategoryName = "Roupa",
+            ImageUrl = "https://github.com/LEOMAZENDA/Microservices_Learning/blob/master/GreekShoping/GreekShoping/ShoppingImages/11_mars.jpg?raw=true",
+        });
+        modelBuilder.Entity<Product>().HasData(new Product
+        {
+            Id = 8,
+            Name = "Casaco Capuchinho Preto",
+            Description = "Casaco de homem para Jovens e Adultos ",
+            Price = new decimal(9580.00),
+            CategoryName = "Roupa",
+            ImageUrl = "https://github.com/LEOMAZENDA/Microservices_Learning/blob/master/GreekShoping/GreekShoping/ShoppingImages/8_moletom_cobra_kay.jpg?raw=true",
+        });
+        modelBuilder.Entity<Product>().HasData(new Product
+        {
+            Id = 9,
+            Name = "Camisola Branca - Dragon Ball Z",
+            Description = "Camisola de homem para Jovens e Adultos ",
+            Price = new decimal(7500.90),
+            CategoryName = "Roupa",
+            ImageUrl = "https://github.com/LEOMAZENDA/Microservices_Learning/blob/master/GreekShoping/GreekShoping/ShoppingImages/13_dragon_ball.jpg?raw=true",
+        });
+
+        base.OnModelCreating(modelBuilder);
+    }
 }

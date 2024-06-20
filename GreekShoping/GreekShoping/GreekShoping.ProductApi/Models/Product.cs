d46b05285bbb07910129f1e8aab18c22ac60ff97
LEOMAZENDA <eng.leomazenda@gmail.com>
2024-06-20 02:45:07 +0100

    Refazendo devido a erros

---
+++ GreekShoping/GreekShoping/GreekShoping.ProductApi/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using GreekShoping.ProductApi.Models.Base;
+
+namespace GreekShoping.ProductApi.Models;
+
+[Table("TbProduct")]
+public class Product : BaseEntity
+{
+    [Required]
+    [Column("name")]
+    [StringLength(150)]
+    public string Name { get; set; }
+
+    [Required]
+    [Column("price")]
+    [Range(1, 10000)]
+    public decimal Price { get; set; }
+
+    [Column("description")]
+    [StringLength(500)]
+    public string Description { get; set; }
+
+    [Required]
+    [Column("category_name")]
+    [StringLength(50)]
+    public string CategoryName { get; set; }
+
+    [Column("image_url")]
+    [StringLength(300)]
+    public string ImageUrl { get; set; }
+}

0ed707686d329421241404f5a2e0a94beadfc8e5
LEOMAZENDA <eng.leomazenda@gmail.com>
2024-06-20 04:23:07 +0100

    Adicionado as imagens do curso

--- GreekShoping/GreekShoping/GreekShoping.ProductApi/Repository/_Product/ProductRepository.cs
+++ GreekShoping/GreekShoping/GreekShoping.ProductApi/Repository/_Product/ProductRepository.cs
@@ -3,64 +3,62 @@ using GreekShoping.ProductApi.Data.ValueObjects;
 using GreekShoping.ProductApi.Models;
 using GreekShoping.ProductApi.Models.Context;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
 
-namespace GreekShoping.ProductApi.Repository._Product
+namespace GreekShoping.ProductApi.Repository._Product;
+
+public class ProductRepository : IProductRepository
 {
-    public class ProductRepository : IProductRepository
+    private readonly MySqlContext _context;
+    private IMapper _mapper;
+
+    public ProductRepository(MySqlContext context, IMapper mapper)
     {
-        private readonly MySqlContext _context;
-        private IMapper _mapper;
+        _context = context;
+        _mapper = mapper;
+    }
 
-        public ProductRepository(MySqlContext context, IMapper mapper)
-        {
-            _context = context;
-            _mapper = mapper;
-        }
+    public async Task<IEnumerable<ProductVO>> FindAll()
+    {
+        List<Product> products = await _context.Products.ToListAsync();
+        return _mapper.Map<List<ProductVO>>(products);
+    }
 
-        public async Task<IEnumerable<ProductVO>> FindAll()
-        {
-            List<Product> products = await _context.Products.ToListAsync();
-            return _mapper.Map<List<ProductVO>>(products);
-        }
+    public async Task<ProductVO> FindById(long id)
+    {
+        Product product = await _context.Products.Where(p => p.Id == id).FirstOrDefaultAsync();
+        return _mapper.Map<ProductVO>(product);
+    }
 
-        public async Task<ProductVO> FindById(long id)
-        {
-            Product product = await _context.Products.Where(p => p.Id == id).FirstOrDefaultAsync();
-            return _mapper.Map<ProductVO>(product);
-        }
+    public async Task<ProductVO> Create(ProductVO vO)
+    {
+        Product product = _mapper.Map<Product>(vO);
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+        return _mapper.Map<ProductVO>(product);
+    }
+    public async Task<ProductVO> Update(ProductVO vO)
+    {
+        Product product = _mapper.Map<Product>(vO);
+        _context.Products.Update(product);
+        await _context.SaveChangesAsync();
+        return _mapper.Map<ProductVO>(product);
+    }
 
-        public async Task<ProductVO> Create(ProductVO vO)
+    public async Task<bool> Delete(long id)
+    {
+        try
         {
-            Product product = _mapper.Map<Product>(vO);
-            _context.Products.Add(product);
+            Product product = await _context.Products.Where(p => p.Id == id)
+                                    .FirstOrDefaultAsync();
+
+            if (product == null) return false;
+            _context.Products.Remove(product);
             await _context.SaveChangesAsync();
-            return _mapper.Map<ProductVO>(product);
+            return true;
         }
-        public async Task<ProductVO> Update(ProductVO vO)
+        catch (Exception)
         {
-            Product product = _mapper.Map<Product>(vO);
-            _context.Products.Update(product);
-            await _context.SaveChangesAsync();
-            return _mapper.Map<ProductVO>(product);
-        }
-
-        public async Task<bool> Delete(long id)
-        {
-            try
-            {
-                Product product = await _context.Products.Where(p => p.Id == id)
-                                        .FirstOrDefaultAsync();
-
-                if (product == null) return false;
-                _context.Products.Remove(product);
-                await _context.SaveChangesAsync();
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
+            return false;
         }
     }
 }

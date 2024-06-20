55666f8c419df013b51ac5a24528eb2c587e3c05
LEOMAZENDA <eng.leomazenda@gmail.com>
2024-06-20 04:43:36 +0100

    Controllers Gets Implementados

---
+++ GreekShoping/GreekShoping/GreekShoping.ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using GreekShoping.ProductApi.Data.ValueObjects;
+using GreekShoping.ProductApi.Repository._Product;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreekShoping.ProductApi.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private IProductRepository _repository;
+    public ProductController(IProductRepository repository)
+    {
+        _repository = repository ?? throw new 
+            ArgumentNullException(nameof(repository));
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+    {
+        var prod = await _repository.FindAll();
+        return Ok(prod);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ProductVO>> FindById(long id)
+    {
+        var prod = await _repository.FindById(id);
+        if (prod == null) return NotFound();
+        return Ok(prod);
+    }
+}

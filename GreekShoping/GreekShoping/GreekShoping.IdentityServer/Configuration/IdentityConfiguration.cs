daaecf63bf149035ff56ca62f7667d9f0f3f439e
LEOMAZENDA <eng.leomazenda@gmail.com>
2024-06-22 04:48:06 +0100

    Trabalhando no Projecto GreekShoping_IdentityServer

---
+++ GreekShoping/GreekShoping/GreekShoping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace GreekShoping.IdentityServer.Configuration;
+
+public static class IdentityConfiguration
+{
+    public const string Admin = "Admin";
+    public const string Costumer = "Costumer";
+
+    // Corrigindo o retorno dos IdentityResources
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Email(),
+            new IdentityResources.Profile(),
+        };
+
+    // Corrigindo o retorno dos ApiScopes
+    public static IEnumerable<ApiScope> ApiScopes =>
+         new List<ApiScope>
+         {
+            new ApiScope("greek_Shoping", "GreekShoping Server"),
+            new ApiScope("read", "Read data"),
+            new ApiScope("write", "Write data"),
+            new ApiScope("delete", "Delete data"),
+         };
+
+    // Corrigindo o Cliente 
+    public static IEnumerable<Client> Client =>
+        new List<Client>
+        {
+             // Use uma strin complexa no lugar de my_super_secret para não comprometer a segurança do token 
+            new Client
+            {
+                ClientId ="Client",
+                ClientSecrets = {new Secret ("my_super_secret".Sha256())},
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedScopes = { "read", "write", "profile"}
+            }
+        };
+
+}
---
+++ GreekShoping/GreekShoping/GreekShoping.IdentityServer/Model/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GreekShoping.IdentityServer.Model;
+
+public class ApplicationUser : IdentityUser
+{
+    private string FirstName { get; set; }
+    private string LastName { get; set; }
+}

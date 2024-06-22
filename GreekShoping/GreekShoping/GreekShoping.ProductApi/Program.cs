7d14d1170fe88d2c59a84e3f67d43c1b7fd41703
LEOMAZENDA <eng.leomazenda@gmail.com>
2024-06-22 07:57:08 +0100

    Pedidio autorização e token nas apis

--- GreekShoping/GreekShoping/GreekShoping.ProductApi/Program.cs
+++ GreekShoping/GreekShoping/GreekShoping.ProductApi/Program.cs
@@ -5,6 +5,7 @@ using GreekShoping.ProductApi.Repository._Product;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,11 +27,11 @@ builder.Services.AddControllers();
 builder.Services.AddAuthentication("Bearer")
     .AddJwtBearer(options =>
     {
-        options.Authority = "https://localhost:4435/"; 
+        options.Authority = "https://localhost:4435/";
         options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateAudience = false
-        }; 
+        };
     });
 builder.Services.AddAuthorization(options =>
 {
@@ -42,7 +43,35 @@ builder.Services.AddAuthorization(options =>
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.EnableAnnotations();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"Enter 'Bearer' [scapece] and your token",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+    {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id ="Bearer"
+            },
+            Scheme = "oauth2",
+            Name = "Bearer",
+            In = ParameterLocation.Header
+        },
+        new List<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 

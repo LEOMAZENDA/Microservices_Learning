bdbb2c3459c3026444c7b2caff6a13ba7334a3bd
LEOMAZENDA <eng.leomazenda@gmail.com>
2024-06-29 04:53:10 +0100

    Criado o VO

---
+++ GreekShoping.OrderAPI/Messeges/CartDetailVO.cs
@@ -0,0 +1,10 @@
+﻿namespace GreekShoping.OrderAPI.Messeges;
+
+public class CartDetailVO
+{
+    public long Id { get; set; }
+    public long CartHeaderId { get; set; }
+    public long ProductId { get; set; }
+    public virtual ProductVO Product { get; set; }
+    public int count { get; set; }
+}
---
+++ GreekShoping.OrderAPI/Messeges/ProductVO.cs
@@ -0,0 +1,11 @@
+﻿namespace GreekShoping.OrderAPI.Messeges;
+
+public class ProductVO 
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Description { get; set; }
+    public string CategoryName { get; set; }
+    public string ImageUrl { get; set; }
+}

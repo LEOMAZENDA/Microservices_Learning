35e8fdbd2528f94e35d29c1bd2d4641a4ad1e79a
Leonildo Vivaldo Mazenda <88457638+LEOMAZENDA@users.noreply.github.com>
2024-07-02 15:37:14 +0100

    Adicionando o Ocelot ou Projecto

---
+++ GreekShoping.APIGateway/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:4435/";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+// Add Ocelot services
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Use Ocelot middleware
+await app.UseOcelot();
+
+app.Run();
+
+
+//using Ocelot.Middleware;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//builder.Services.AddControllers();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.UseOcelot();
+
+//app.Run();
